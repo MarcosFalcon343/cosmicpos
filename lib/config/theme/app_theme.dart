59a55680f606d40fc311484665811cc96f8482d2
MarcosFalcon343 <marcosfalcon2003@gmail.com>
2023-09-24 19:02:12 -0600

    Create app drawer

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF003459);
+
+class AppTheme{
+  final bool darkMode;
+
+  AppTheme({this.darkMode = false});
+
+  ThemeData theme(){
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _customColor,
+      brightness: (darkMode) ? Brightness.dark : Brightness.light,
+    );
+  }
+}
